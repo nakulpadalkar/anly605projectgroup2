7e1ff9d1f74a9d3bd249f2282a8870174833051b
Jingyi Wang <jw1927@georgetown.edu>
2021-12-01 19:21:43 -0500

    Project Application

--- app/app.py
+++ app/app.py
@@ -23,8 +23,6 @@ app = Flask(__name__)
 
 # @app.route("/") # Start here
 @app.route("/",methods=['GET','POST']) # We need to change the first line to include GET and POST methods
-
-
 def hello_world():
     request_type_str = request.method
     if request_type_str=='GET':
